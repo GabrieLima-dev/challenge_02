cad09ceee536a3b191e16299869500baf10f9603
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-04-23 12:07:30 -0300

    first commit

---
+++ lib/challenge02.ex
@@ -0,0 +1,18 @@
+defmodule Challenge02 do
+  @moduledoc """
+  Documentation for `Challenge02`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Challenge02.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/challenge02_test.exs
@@ -0,0 +1,8 @@
+defmodule Challenge02Test do
+  use ExUnit.Case
+  doctest Challenge02
+
+  test "greets the world" do
+    assert Challenge02.hello() == :world
+  end
+end
